6c244a179f40cdd4ddaa127b7528a068fb3d6dc1
Radmir Gurkov <ragux0@gmail.com>
2022-01-09 23:32:12 +0100

    First part of 3rd task

--- 3/index.js
+++ 3/index.js
@@ -1,21 +1,56 @@
 const readFile = require("../readFile");
 
-readFile("./testInput.txt").then((data) => {
-  const countMap = {};
-  const lines = data.split("\n").map((value) => value.split("").map(Number));
+const rotateMatrix = (matrix) => {
+  const result = [];
 
-  const binaryLength = lines[0].length;
-
-  for (const line of lines) {
-    console.log(line);
-    for (let i = 0; i < binaryLength; i++) {
-      console.log("value", line[i]);
-      if (countMap[i] != undefined) {
-        countMap[i] += line[i];
-      } else {
-        countMap[i] = 0;
+  for (const rowIdx in matrix) {
+    const line = matrix[rowIdx];
+    for (const valueIdx in matrix[rowIdx]) {
+      if (!result[valueIdx]) {
+        result[valueIdx] = [];
       }
+      result[valueIdx][rowIdx] = line[valueIdx];
     }
   }
-  console.log(countMap);
+  return result;
+};
+
+const binaryToDecimal = (array) => parseInt(array.join(""), 2);
+
+readFile("./input.txt").then((data) => {
+  const lines = data.split("\n").map((value) => value.split("").map(Number));
+
+  const rotatedMatrix = rotateMatrix(lines);
+
+  const gammaRate = [];
+  const epsilonRate = [];
+
+  let countsMap = {};
+
+  for (const row of rotatedMatrix) {
+    for (const value of row) {
+      if (!countsMap[value]) {
+        countsMap[value] = 0;
+      }
+      countsMap[value] += 1;
+    }
+    const mostCommon = Math.max(...Object.values(countsMap));
+    for (const key of Object.keys(countsMap)) {
+      const parsedKey = Number(key);
+      const value = countsMap[parsedKey];
+      if (value == mostCommon) {
+        gammaRate.push(parsedKey);
+      } else {
+        epsilonRate.push(parsedKey);
+      }
+    }
+    countsMap = {};
+  }
+
+  const decimalGamma = binaryToDecimal(gammaRate);
+  const decimalEpsilon = binaryToDecimal(epsilonRate);
+
+  console.log(decimalGamma);
+  console.log(decimalEpsilon);
+  console.log("result", decimalGamma * decimalEpsilon);
 });
